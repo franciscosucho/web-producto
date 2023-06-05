5642ba099fcd719c02853ea303f4bf38b9df5a4f
franciscosucho <110209358+franciscosucho@users.noreply.github.com>
2023-06-04 23:03:30 -0300

    casi  terminado 4-6

---
+++ index.js
@@ -0,0 +1,12 @@
+var btn = document.querySelector(".sign-in");
+var ver = document.querySelector(".ver");
+var get = document.querySelector(".get");
+ver.addEventListener("click", function () {
+    window.open("https://www.razer.com/gaming-headsets/razer-blackshark-v2/RZ04-03230100-R3M1#specs", "_blank");
+});
+get.addEventListener("click", function () {
+    window.open("https://www.razer.com/gaming-headsets/razer-blackshark-v2/RZ04-03230100-R3M1#specs", "_blank");
+});
+btn.addEventListener("click", function () {
+    window.open("https://github.com/franciscosucho/formulario/blob/main/index.html", "_blank");
+});
